f1de6cb7d7833a277bfd9f9e79091aa946a7c054
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-05-20 17:52:27 -0400

    POST ROUTE TEST

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,6 +7,67 @@ const request = supertest(app);
 
 describe('API Routes', () => {
   afterAll(async () => {
-    return client.
-  })
-})
+    return client.end();
+  });
+
+  describe('favorites', () => {
+    let user;
+    let user2;
+
+    beforeAll(async () => {
+      execSync('npm run recreate-tables');
+
+      const response = await request
+        .post('/api/auth/signup')
+        .send({
+          name: 'Merlin',
+          email: 'merlin@aol.com',
+          password: 'magicshit'
+        });
+
+      expect(response.status).toBe(200);
+
+      user = response.body;
+
+      const response2 = await request
+        .post('/api/auth/signup')
+        .send({
+          name: 'Arthur',
+          email: 'Arthur@msn.com',
+          password: 'kingshit'
+        });
+
+      expect(response2.status).toBe(200);
+
+      user2 = response2.body;
+    });
+
+    let favorite = {
+      id: expect.any(Number),
+      movieId: 550,
+      title: 'Fight Club',
+      year: '1999-10-15',
+      genre: 'Drama',
+      rating: 8.4,
+      img: '/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg'
+    };
+
+    it('POST fave to /api/favorites', async () => {
+
+      const response = await request
+        .post('/api/favorites')
+        .set('Authorization', user.token)
+        .send(favorite);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual({ 
+        userId: user.id,
+        ...favorite
+      });
+
+      favorite = response.body;
+    
+    });
+
+  });
+});
